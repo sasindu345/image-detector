f7548250af616ee8ebcbc1a08da9e9a86ba5ced9
wijewardana S.J. <wijewardanasj.23@uom.lk>
2026-04-01 00:34:11 +0530

    Reorganize project structure: move notebook, add training and data dirs
    - Move image_classification.ipynb to notebooks/
    - Add training/ scaffold for future retraining and active learning
    - Add data/ directory (git-ignored) for dataset storage

---
+++ training/train.py
@@ -0,0 +1,8 @@
+"""
+Training script placeholder.
+
+Future enhancements:
+- Retrain with higher-resolution images
+- Experiment with transfer learning (EfficientNet, ResNet)
+- Add active learning pipeline to label uncertain predictions
+"""
